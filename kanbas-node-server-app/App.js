c1aad9a6b4add8121ec195a8d6ba5984313ecfce
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-04-16 16:22:26 -0400

    bug fixes for render deployment

--- kanbas-node-server-app/App.js
+++ kanbas-node-server-app/App.js
@@ -9,29 +9,38 @@ import UserRoutes from "./Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
+axios.defaults.withCredentials = true;
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        proxy: true,
+        cookie: {
+            sameSite: "none",
+            secure: true,
+            domain: "kanbas-node-server-app-sp24-cs5610-02-l55v.onrender.com",
+        },
     })
-   );   
-   const sessionOptions = {
+);
+const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
+};
+if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
     };
-  }
-  app.use(session(sessionOptions));  
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
